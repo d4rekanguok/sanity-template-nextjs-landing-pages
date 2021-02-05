ca2b42a4ff5602f57db17c7abdd42d56586f26e3
Derek Nguyen <derek@penandpillow.com>
2021-02-06 05:00:28 +0900

    feat: generate sitemap with api

--- template/web/pages/api/sitemap.js
+++ template/web/pages/api/sitemap.js
@@ -1,7 +1,6 @@
-const { SitemapStream, streamToPromise } = require('sitemap')
-const fs = require('fs/promises')
-const client = require('./client')
-const groq = require('groq')
+import { SitemapStream, streamToPromise } from 'sitemap'
+import groq from 'groq'
+import client from '../../client'
 
 const configQuery = groq`*[_id == "global-config"] {url}[0]`
 
@@ -20,37 +19,45 @@ const routeQuery = groq`
 }
 `
 
-Promise.all([configQuery, routeQuery].map(query => client.fetch(query)))
-  .then(results => {
-    const [ config, routes ] = results
+export default async (req, res) => {
+  try {
+    const [config, routes] = await Promise.all([configQuery, routeQuery].map(query => client.fetch(query)))
+    if (Object.keys(config).length === 0 || Object.keys(routes).length === 0) {
+      throw new Error('No data')
+    }
 
     const smStream = new SitemapStream({ hostname: config.url })
-
+  
     routes.forEach(route => {
       const { slug, disallowRobots = false, includeInSitemap = true, _updatedAt } = route
-
+  
       if (!includeInSitemap || disallowRobots) {
         return
       }
-
+  
       smStream.write({
         url: slug,
         // Uncomment this if you need lastmod i.e not evergreen content
         // lastmod: new Date(_updatedAt),
-
+  
         // other config
         // changefreq: 'daily',
         // priority: 0.5,
-      });
-    });
-
+      })
+    })
+  
     smStream.end()
-    return streamToPromise(smStream)
-  })
-  .then(sitemapRes => {
-    const output = sitemapRes.toString()
-    return fs.writeFile(`./out/sitemap.xml`, output)
-  })
-  .then(() => {
-    console.log(`sitemap.xml updated`)
-  })
+    const output = (await streamToPromise(smStream)).toString()
+  
+    res.writeHead(200, {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 's-maxage=86400',
+    })
+    res.end(output)
+
+  } catch (e) {
+    console.log(e)
+    res.status(500)
+    res.end()
+  }
+}
